ef46a4e570ee9e4f7989f809d8c174b513ec4923
Karol Moroz <k.j.moroz@gmail.com>
2021-09-09 15:44:34 +0200

    Set up bulma

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css";
+import "../css/app.sass";
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
